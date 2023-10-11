7bb024c0419132985fb469053f4d7d5166d85379
marwan-pixel <marwanhadid6@gmail.com>
2023-10-12 06:05:18 +0700

    Upload project maruudrawing to Github

---
+++ app/Views/Home.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <nav class="p-6 flex flex-row">
+        <div class="basis-3/4">
+            <p class="font-sans text-xl">MaruuDrawing</p>
+        </div>
+        <div class="basis-1/4 flex justify-around ">
+            <p class="font-sans text-md">About</p>
+            <p class="font-sans text-md">Paintings</p>
+            <p class="font-sans text-md">Contact</p>
+        </div>
+    </nav>
+
+    <div class="content">
+        <div class="header flex mx-12 mt-10" style="background-color: cadetblue;" >
+            <img class="mr-12" style="width: 30%;" src="pictures/myPhotos.webp" alt="">
+            <div class="ml-12 my-10" style="background-color: blue;">
+                <p class="font-sans text-xl text-left">Amateur Artist, Programmer.</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>

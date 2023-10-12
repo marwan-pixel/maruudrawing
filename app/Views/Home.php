06c17a1b65adfe3d03f8d5d21e18d6dc7dae4f9f
marwan-pixel <marwanhadid6@gmail.com>
2023-10-12 07:35:40 +0700

    Upload project maruudrawing to Github

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -4,26 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
     <nav class="p-6 flex flex-row">
         <div class="basis-3/4">
-            <p class="font-sans text-xl">MaruuDrawing</p>
+            <p class=" text-2xl">MaruuDrawing</p>
         </div>
         <div class="basis-1/4 flex justify-around ">
-            <p class="font-sans text-md">About</p>
-            <p class="font-sans text-md">Paintings</p>
-            <p class="font-sans text-md">Contact</p>
+            <p class=" text-md">About</p>
+            <p class=" text-md">Paintings</p>
+            <p class=" text-md">Contact</p>
         </div>
     </nav>
 
     <div class="content">
-        <div class="header flex mx-12 mt-10" style="background-color: cadetblue;" >
-            <img class="mr-12" style="width: 30%;" src="pictures/myPhotos.webp" alt="">
-            <div class="ml-12 my-10" style="background-color: blue;">
-                <p class="font-sans text-xl text-left">Amateur Artist, Programmer.</p>
+        <div class="header flex mx-12 mt-10" style="" >
+            <img class="mr-12 basis-1/2 object-none h-100 w-50" src="pictures/myPhotos.webp" alt="">
+            <div class="ml-12 my-10 basis-1/2" style="">
+                <p class="text-lg text-left">Artist, Programmer.</p>
+                <p class="mt-6 text-7xl">Loving to draw and sketch someone or something then post it</p>
             </div>
         </div>
     </div>
